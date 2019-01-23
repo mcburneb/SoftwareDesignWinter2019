2aaad43c7dd431ba11247bcf81ba3374753b3335
Brianna McBurney <mcburneb@sheridancollege.ca>
2019-01-23 11:48:14 -0500

    Revert "my 2nd commit"
    This reverts commit abce51c17444c52417ac95b9fb5f1c759768aeff.

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
@@ -13,7 +13,6 @@ public class StudentArray
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
         StudentDemo [] studentList = new StudentDemo[5];
         StudentDemo student1 = new StudentDemo("1", "Jim Halpern");
         studentList[0] = student1;
